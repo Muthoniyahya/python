c6d8fcec1d9784e676995a81e2aa66ed2ae08276
student <Muthoniyahya@gmail.com>
2021-05-24 20:21:37 +0300

    first commit

--- bank.py
+++ bank.py
@@ -1,12 +1,81 @@
+# class BankAccount:
+  
+#     def __init__(self,service,salary,phone_number,name):
+#      self.services=services
+#      self.salary=salary
+#      self.phone_number=phone_number
+#      self.balance=0
+#      self.name=name
+
+#     def opinion(self):
+#         return f"Equity offers one of the best {self.services}"
+
+#     def income(self):
+#         return f"Employers earn more than enough money for their upkeep ${self.salary}."
+
+#     def show_balance(self):
+#         return f"Hello {self.name} your balance is {self.balance}"
+
+#     def deposit(self,amount):
+#         self.balance += amount
+#         return self.show_balance()
+#     def withdraw(self,amount)
+#         if amount >self.balance
+#         return f
+
+
+    
+
 class BankAccount:
-    Brand="Equity"
-    def __init__(self,services,salary):
-     self.services=services
-     self.salary=salary
+  
+    def __init__(self,phone_number,name):
+     self.phone_number=phone_number
+     self.balance=0
+     self.name=name
+     self.borrow=0
 
-    def opinion(self):
-        return f"Equity offers one of the best {self.services}"
+    def show_balance(self):
+        return f"Hello {self.name} your balance is {self.balance}"
 
-    def income(self):
-        return f"Employers earn more than enough money for their upkeep ${self.salary}."
+    def deposit(self,amount):
+        if amount > 0:
+           self.balance += amount
+           return self.show_balance()
+
+        else:
+            
+            return f"Dear customer you have insufficient funds your balance is {self.balance}"
+
+    def withdraw(self,amount):
+        if amount > self.balance:
+            return f"Hello your account balance is {self.balance} you cannot withdraw {amount}"
+
+    def loan(self,amount):
+        return f"Hello your loan balance is {amount}"
+
+    def repayloan(self,amount):
+        return f"Hello your loan balance is {amount}"
+        
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- student.py
+++ student.py
@@ -1,12 +1,23 @@
 class Student:
     school="Akirachix"
-    def __init__(self,name,age):
-     self.name=name
+    def __init__(self,Fist_name,Last_name,age,year_of_birth):
+     self.First_name=First_name
+     self.Last_name=Last_name
      self.age=age
+     self.year_of_birth=year_of_birth
+
+     def greet(self):
+         return f"Hello {self.First_name} welcome to {self.school}"
 
     def speak(self):
         return f"Hello class, my name is {self.name}"
 
     def year_of_birth(self):
-        return f"Iman was born in {2021-self.age}."
+        return f"Iman is {self.age} years old"
+    def greetings(self):
+        return f "Hello  you were born in {self.age}"
+    
+    
+
+
 
